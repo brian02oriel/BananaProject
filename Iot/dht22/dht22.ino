5aea4f79e67d0f1c792df500b06eda3bcf92c718
brian02oriel <brian.nieto1@utp.ac.pa>
2019-10-24 15:22:43 -0500

    deleting arduino leds

--- Iot/dht22/dht22.ino
+++ Iot/dht22/dht22.ino
@@ -13,33 +13,22 @@
 
 DHT dht = DHT(dataPin, DHTTYPE);
 
-Timer t1, t2;
+Timer t1;
 
 const int capacity = JSON_OBJECT_SIZE(3);
 StaticJsonDocument<capacity> data;
-const int pinLedA = 2;
-const int pinLedB = 3;
+
 
 void setup() {
   // put your setup code here, to run once:
-  
-  pinMode(pinLedA, OUTPUT);
-  pinMode(pinLedB, OUTPUT);
   Serial.begin(9600);
   dht.begin();
-  int bright = t1.every(1000, Bright);
-  int sensor = t2.every(1000, Sensor);
+  int sensor = t1.every(1000, Sensor);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   t1.update();
-  t2.update();
-}
-
-void Bright(){
-    digitalWrite(pinLedA, HIGH);
-    digitalWrite(pinLedB, HIGH);
 }
 
 void Sensor(){
