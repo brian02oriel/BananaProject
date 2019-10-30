7040d111b5417af95d2218b1c0fe4e1463d917da
brian02oriel <brian.nieto1@utp.ac.pa>
2019-10-29 22:04:29 -0500

    improving timing

--- Iot/timerloop.py
+++ Iot/timerloop.py
@@ -7,13 +7,16 @@ timer = 10
 count = 0
 
 while True:
-        now = datetime.now()
-        current_time = now.strftime('%H:%M:%S')
-        with picamera.PiCamera() as picam:
-                picname = 'pic'+ str(count) +'.jpg'
-                picam.capture('images/'+picname)
-                picam.close()
-                MongoConnection('images/'+picname)
-                time.sleep(timer)
-                count += 1
+        now = datetime.now().time()
+        current_time = now.strftime("%H:%M:%S")
+        if(current_time == "00:00:00" or current_time == "06:00:00"  or current_time == "12:00:00" or current_time == "18:00:00"):
+                print("Activating Camera")
+                with picamera.PiCamera() as picam:
+                        picname = 'pic'+ str(count) +'.jpg'
+                        picam.capture('images/'+picname)
+                        MongoConnection('images/'+picname)
+                        #time.sleep(timer)
+                        count += 1
+                        picam.close()
+                print("Closing Camera")
 
