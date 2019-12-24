231212e127d32a32f1131be7b76be10a1bde993b
brian02oriel <brian.nieto1@utp.ac.pa>
2019-12-24 16:43:47 -0500

    tests

--- Iot/timerloop.py
+++ Iot/timerloop.py
@@ -3,20 +3,20 @@ import time
 from datetime import datetime
 import picamera
 from mongoAtlas import MongoConnection 
-timer = 10
+timer = 100
 count = 0
 
 while True:
         now = datetime.now().time()
-        current_time = now.strftime("%H:%M:%S")
-        if(current_time == "00:00:00" or current_time == "06:00:00"  or current_time == "12:00:00" or current_time == "18:00:00"):
-                print("Activating Camera")
-                with picamera.PiCamera() as picam:
-                        picname = 'pic'+ str(count) +'.jpg'
-                        picam.capture('images/'+picname)
-                        MongoConnection('images/'+picname)
-                        #time.sleep(timer)
-                        count += 1
-                        picam.close()
-                print("Closing Camera")
+        #current_time = now.strftime("%H:%M:%S")
+        #if(current_time == "00:00:00" or current_time == "06:00:00"  or current_time == "12:00:00" or current_time == "18:00:00"):
+        print("Activating Camera")
+        with picamera.PiCamera() as picam:
+                picname = 'pic'+ str(count) +'.jpg'
+                picam.capture('images/'+picname)
+                MongoConnection('images/'+picname)
+                #time.sleep(timer)
+                count += 1
+                picam.close()
+        print("Closing Camera")
 
