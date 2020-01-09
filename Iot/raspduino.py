1b7d48943c8de6e20a8e7833ddc413ff7d16a559
brian02oriel <brian.nieto1@utp.ac.pa>
2020-01-08 22:46:33 -0500

    adding try

--- Iot/raspduino.py
+++ Iot/raspduino.py
@@ -11,25 +11,14 @@ def SerialConnection():
 		while 1:
 			try:
 				data_string = ser.readline()
-			except ValueError:
-				print("Error found: ", ValueError)
-				data_string = '{"temperature": 0, "humidity": 0}'
-			
-			try:
 				data_string = data_string.decode("utf-8")
-			except ValueError:
-				print("Error found: ", ValueError)
-				data_string = '{"temperature": 0, "humidity": 0}'
-				
-				
-			print("coming data: ", data_string)
-			
-			try:
 				data_json = json.loads(data_string)
 			except ValueError:
 				print("Error found: ", ValueError)
-				data_json = '{"temperature": 0, "humidity": 0}'
-				
+				data_string = '{"temperature": 0, "humidity": 0}'
+				data_json = json.loads(data_string)
+			
+			print("coming data: ", data_string)	
 			print("JSON: ",data_json)
 			return data_json
 	else:
